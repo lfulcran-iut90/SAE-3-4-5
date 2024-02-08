09ea3091dc14aed8dba7b1d4a9a488fee4a984fa
lfulcran-iut90 <lucas.fulcrand@edu.univ-fcomte.fr>
2024-02-08 10:05:44 +0000

    ajout .env

--- connexion_db.py
+++ connexion_db.py
@@ -2,17 +2,19 @@ from flask import Flask, request, render_template, redirect, url_for, abort, fla
 
 import pymysql.cursors
 
+import os                                 # à ajouter
+from dotenv import load_dotenv            # à ajouter
+#load_dotenv()
+project_folder = os.path.expanduser('~/SAE-3-4-5')  # adjust as appropriate (avec le dossier où se trouve le fichier .env et app.py)
+load_dotenv(os.path.join(project_folder, '.env'))
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        #
-        db = g._database = pymysql.connect(
-            host="localhost",
-            # host="serveurmysql",
-            user="jlancon",
-            password="2407",
-            database="BDD_jlanconsae345",
+    if 'db' not in g:
+        g.db =  pymysql.connect(
+            host=os.environ.get("HOST"),                # à modifier
+            user=os.environ.get("LOGIN"),               # à modifier
+            password=os.environ.get("PASSWORD"),        # à modifier
+            database=os.environ.get("DATABASE"),        # à modifier
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
-    return db
+    return g.db
